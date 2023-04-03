3640b8408e03dc0ac4b100fd9f935cc4029fab3b
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-04-03 00:14:47 -0400

    initial setup

---
+++ website/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier',
+    'react-app',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'prettier'],
+  overrides: [],
+};
---
+++ website/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>indigo-turtle-blog title!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        error magnam dolorum laboriosam, modi veniam corrupti impedit incidunt
+        eius illo voluptates libero architecto similique eveniet dignissimos qui
+        maiores debitis quis atque commodi perferendis excepturi dicta
+        voluptatibus eligendi? Assumenda modi ab possimus, amet adipisci
+        excepturi quam atque esse non id cumque!
+      </p>
+    </div>
+  );
+}
+
+export default App;
