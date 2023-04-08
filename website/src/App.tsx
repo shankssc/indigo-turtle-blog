03f8722cf88d6cbfcc99d2a7993d978a61e2ebc2
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-04-08 13:30:19 -0400

    pulled changes and added comments

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { AccountPage } from './components/AccountPage'
-import { Home } from './components/index'
+import { AccountPage } from './components/AccountPage';
+import { Home } from './components/index';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-      <Router>
-            <div className='App'>
-              <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/account' element={<AccountPage/>}/>
-              </Routes>
-            </div>
-          </Router>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />{' '}
+          {/* TODO: "/" should be routing to AuthPage */}
+          <Route path="/account" element={<AccountPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
