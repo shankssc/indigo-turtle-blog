9021ea35a49c1468d5c2ce0f53cba2e71e258cd2
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-04-18 23:28:57 -0400

    commented out CreatePost

--- website/src/components/createPost.tsx
+++ website/src/components/createPost.tsx
@@ -1,58 +1,118 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { myContext } from 'component/Context';
 
-interface FormData {
-  postTitle: string;
-  postContent: string;
-  tags: string[];
-}
+// interface FormData {
+//   postTitle: string;
+//   postContent: string;
+//   tags: string[];
+// }
 
-const initialFormData: FormData = {
-  postTitle: '',
-  postContent: '',
-  tags: [],
-};
+// const initialFormData: FormData = {
+//   postTitle: '',
+//   postContent: '',
+//   tags: [],
+// };
 
-const handleInputChange = (
-  setFormData: React.Dispatch<React.SetStateAction<FormData>>,
-  formData: FormData,
-  event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-): void => {
-  const { name, value } = event.target;
-  setFormData({ ...formData, [name]: value });
-};
+// const handleInputChange = (
+//   setFormData: React.Dispatch<React.SetStateAction<FormData>>,
+//   formData: FormData,
+//   event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+// ): void => {
+//   const { name, value } = event.target;
+//   setFormData({ ...formData, [name]: value });
+// };
 
-const handleSelectChange = (
-  setFormData: React.Dispatch<React.SetStateAction<FormData>>,
-  formData: FormData,
-  event: React.ChangeEvent<HTMLSelectElement>
-): void => {
-  const options = event.target.options;
-  const selectedOptions: string[] = [];
-  for (let i = 0; i < options.length; i++) {
-    if (options[i].selected) {
-      selectedOptions.push(options[i].value);
-    }
-  }
-  setFormData({ ...formData, tags: selectedOptions });
-};
+// const handleSelectChange = (
+//   setFormData: React.Dispatch<React.SetStateAction<FormData>>,
+//   formData: FormData,
+//   event: React.ChangeEvent<HTMLSelectElement>
+// ): void => {
+//   const options = event.target.options;
+//   const selectedOptions: string[] = [];
+//   for (let i = 0; i < options.length; i++) {
+//     if (options[i].selected) {
+//       selectedOptions.push(options[i].value);
+//     }
+//   }
+//   setFormData({ ...formData, tags: selectedOptions });
+// };
 
-function CreatePost(): JSX.Element {
-  const [formData, setFormData] = useState<FormData>(initialFormData);
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-    // Prevent the browser from reloading the page
-    event.preventDefault();
+// function CreatePost(): JSX.Element {
+//   const [formData, setFormData] = useState<FormData>(initialFormData);
 
-    // Read the form data
-    const form = event.target as HTMLFormElement;
-    const formData = new FormData(form);
+//   function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+//     // Prevent the browser from reloading the page
+//     event.preventDefault();
 
-    // pass formData as a fetch body directly:
-    // fetch('/some-api', { method: form.method, body: formData }): void;
-  }
+//     // Read the form data
+//     const form = event.target as HTMLFormElement;
+//     const formData = new FormData(form);
 
-  function handleCancel(): void {
-    // redirect to posts page
-  }
+//     // pass formData as a fetch body directly:
+//     // fetch('/some-api', { method: form.method, body: formData }): void;
+//   }
+
+//   function handleCancel(): void {
+//     // redirect to posts page
+//   }
+
+//   return (
+//     <form method="post" onSubmit={handleSubmit}>
+//       <label>
+//         Title:
+//         <input
+//           name="postTitle"
+//           defaultValue="Title"
+//           value={formData.postTitle}
+//           onChange={handleInputChange.bind(null, setFormData, formData)}
+//         />
+//       </label>
+//       <label>
+//         Edit your post:
+//         <textarea
+//           name="postContent"
+//           placeholder="deep thoughts"
+//           rows={4}
+//           cols={40}
+//           value={formData.postContent}
+//           onChange={handleInputChange.bind(null, setFormData, formData)}
+//         />
+//       </label>
+//       <label>
+//         Select post tags:
+//         <select
+//           name="tags"
+//           id="tags"
+//           multiple
+//           required
+//           value={formData.tags}
+//           onChange={handleSelectChange.bind(null, setFormData, formData)}
+//         >
+//           <option value="travel">Travel</option>
+//           <option value="cooking">Cooking</option>
+//           <option value="hobbies">Hobbies</option>
+//           <option value="music">Music</option>
+//           <option value="career">Career</option>
+//           <option value="programming">Programming</option>
+//         </select>
+//       </label>
+//       <p className="tagDisplay"></p>
+//       <hr />
+//       <button onClick={handleCancel}>Cancel</button>
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export { CreatePost };
+
+export function CreatePost(): JSX.Element {
+  const ctx = useContext(myContext);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [tags, setTags] = useState<string[]>([]);
+
+  const handleSubmit = (): void => {};
 
   return (
     <form method="post" onSubmit={handleSubmit}>
@@ -61,7 +121,7 @@ function CreatePost(): JSX.Element {
         <input
           name="postTitle"
           defaultValue="Title"
-          value={formData.postTitle}
+          value={title}
           onChange={handleInputChange.bind(null, setFormData, formData)}
         />
       </label>
@@ -101,5 +161,3 @@ function CreatePost(): JSX.Element {
     </form>
   );
 }
-
-export { CreatePost };
